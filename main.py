b7d4801863a5b0d9cf7ebc2e051f5a766baa007d
mazuluagac <142950789+mazuluagac@users.noreply.github.com>
2025-05-06 22:07:04 -0500

    Integrar flujo completo de ejecución en main.py

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 from estudiantes.registro import cargar_estudiantes, ordenar_y_mostrar_estudiantes, calcular_promedio
 
 def main():
+    # Ruta del archivo CSV
     ruta = "estudiantes/estudiantes.csv"
+    
+    # Cargar estudiantes desde el archivo
     estudiantes = cargar_estudiantes(ruta)
     
+    # Verificar si hay estudiantes válidos
     if not estudiantes:
         print("No se encontraron estudiantes válidos.")
         return
     
+    # Mostrar la lista de estudiantes ordenados
     print("\nLista de estudiantes:")
     ordenar_y_mostrar_estudiantes(estudiantes)
     
-    #print("\nCálculo del promedio:")
+    # Calcular y mostrar el promedio de las notas
     print("\nPromedio de notas:")
     calcular_promedio(estudiantes)
 
 if __name__ == "__main__":
     main()
-    input("\nPresiona Enter para salir...")
